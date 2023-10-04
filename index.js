a97912f4587b9f8fb87b8299ebd9c33726454ab3
yordanos woldeyes <107896300+yordanosW@users.noreply.github.com>
2023-10-03 21:50:19 -0700

    create  a function

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newimage(url,left,bottom){
+
+    let newImg = document.createElement('img')
+    newImg.src = url
+    newImg.style.position ='fixed'
+   newImg.style.left = left
+    newImg.style.bottom =bottom
+    document.body.append(newImg)
+}
+
+newimage('assets/pine-tree.png','450px','200px')
+newimage('assets/green-character.gif','100px','100px')
+newimage('assets/pillar.png','350px', '100px')
+newimage('assets/crate.png','150px','200px')
+newimage('assets/well.png','500px','425px')
+
+function newItem(url,left,bottom){
+    let item = document.createElement('img')
+    item.src = url
+    item.position = 'fixed'
+    item.left = left
+    item.bottom = bottom
+    document.body.append(item)
+    
+    item.addEventListener('dblclick', function(){
+        item.remove()
+    })  
+
+
+
+}
+newItem('assets/sword.png','500px','405px')
+newItem('assets/shield.png','165px','185px')
+newItem('assets/staff.png','600px','100px')
